657f3a70e14d37e1bfc2f6ad9a258a756fb658d7
Sam Garson <samtgarson@gmail.com>
2020-11-04 11:33:40 +0000

    Handle 100 bags of corn

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ describe('FarmerCrossing', () => {
         expect(instance).toBeInstanceOf(FarmerCrossing)
     })
 
-    it('when theres one bag of corn the price is 0p', () => {
+    it('when there is one bag of corn the price is 0p', () => {
         const totalPrice = instance.calculatePriceOfCrossing(0);
 
         expect(totalPrice).toEqual(0);
@@ -21,5 +21,11 @@ describe('FarmerCrossing', () => {
         const totalPrice = instance.calculatePriceOfCrossing(2);
         
         expect(totalPrice).toEqual(75);
+    })
+
+    it('when there are 100 bags of corn the price is 4975p', () => {      
+        const totalPrice = instance.calculatePriceOfCrossing(100);
+        
+        expect(totalPrice).toEqual(4975);
     })
 })
