71bdfb663b94087cc35f73f47e118361bf2a76db
Tim Hacker <tim.hacker@madetech.com>
2020-11-05 16:18:15 +0000

    pounds!

--- src/components/plan.js
+++ src/components/plan.js
@@ -64,7 +64,7 @@ export const Plan = ({ data: { corn, geese } }) => {
       currency: "GBP",
     });
 
-    return formatter.format(cost);
+    return formatter.format(cost / 100);
   }, [cost]);
 
   if (!plan) return null;
